127d73d6811294d5ad7b7928fbf9521d96ea6cbe
Calvin Mlynarczyk <calvin_mlynarczyk@ml1.net>
2021-10-13 21:23:09 -0400

    Create user name post route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { AppService } from "./app.service";
 
 @Controller()
@@ -9,5 +9,10 @@ export class AppController {
   root(): string {
     console.log("Request received");
     return this.appService.getHello();
+  }
+
+  @Post('user')
+  addUser(@Body('name') name: string): void {
+    this.appService.addUser(name);
   }
 }
