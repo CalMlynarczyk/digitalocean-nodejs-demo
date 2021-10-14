fcaa0ae9ce66224a2c8ce3ded7e68ed3d6c6621f
Calvin Mlynarczyk <calvin_mlynarczyk@ml1.net>
2021-10-13 21:09:49 -0400

    Format

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,9 @@
-import {Pool} from 'pg';
-import {env} from './process';
+import { Pool } from "pg";
+import { env } from "./process";
 
 export const pool = new Pool({
   connectionString: env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
