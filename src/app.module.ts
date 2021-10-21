16e15d8f11860898b4f54dfa01a61e7f2dd73b63
Calvin Mlynarczyk <calvin_mlynarczyk@ml1.net>
2021-10-20 20:07:32 -0400

    Add Sentry error logging integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,37 @@
 import { Module } from "@nestjs/common";
+import { SentryModule } from "@ntegral/nestjs-sentry";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+import { LogLevel } from "@sentry/types";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
+import { env } from "./lib/process";
 import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    SentryModule.forRoot({
+      dsn: env.SENTRY_DSN(),
+      debug: true,
+      environment: "dev",
+      // release: null, // must create a release in sentry.io dashboard
+      logLevel: LogLevel.Debug, //based on sentry.io loglevel //
+      integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        // new Tracing.Integrations.Express(),
+        // new Tracing.Integrations.Postgres(),
+      ],
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
