108979274d543f9b8ce2f832246bd395756e4955
Calvin Mlynarczyk <calvin_mlynarczyk@ml1.net>
2021-11-16 21:53:11 -0500

    Update unit tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -3,6 +3,7 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 describe("AppController", () => {
+  let appService: AppService;
   let appController: AppController;
 
   beforeEach(async () => {
@@ -11,12 +12,17 @@ describe("AppController", () => {
       providers: [AppService],
     }).compile();
 
+    appService = app.get<AppService>(AppService);
     appController = app.get<AppController>(AppController);
   });
 
   describe("root", () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.root()).toBe("Hello World!");
+    it("should return 'Hello...'", () => {
+      jest
+        .spyOn(appService, "getHello")
+        .mockImplementation(() => "Hello guys!");
+
+      expect(appController.root()).toBe("Hello guys!");
     });
   });
 });
